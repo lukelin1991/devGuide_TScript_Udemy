9883f62141b4d462a0d2d1bc15ec419139755756
lukelin1991 <lukelin1991@yahoo.com>
2020-07-06 13:29:01 -0400

    more functional TypeScript

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -28,3 +28,30 @@ const logNumber: (i: number) => void = (i: number) => {
   // annotation is everything between the : and before the =
   console.log(i); // everything after the = is the actual arrow function.
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
