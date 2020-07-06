6b4de7b433436c6b33608fc07e5261d7b9f8e2b5
lukelin1991 <lukelin1991@yahoo.com>
2020-07-06 12:19:57 -0400

    object literal classes and arrays

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,9 +1,24 @@
-let apples: number = 5; // annotated, only numbers can be assigned to the variable apples.
-let speed: string = "fast";
 let hasName: boolean = true;
+let apples: number = 5; // annotated, only numbers can be assigned to the variable apples.
+let speed: string = "fast"; // only strings can be here.
 
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
