6fb6a91e22f6cbc04e64828848e1ef247b4d3041
lukelin1991 <lukelin1991@yahoo.com>
2020-07-07 14:03:14 -0400

    learning return annotation

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,8 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtract = (a: number, b: number): number => {
+  // always use a "return" annotation.
+  return a - b;
+};
