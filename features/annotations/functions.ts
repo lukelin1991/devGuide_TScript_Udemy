8dc22911ddb4b8fa9b62f4264bf4cd13fa337686
lukelin1991 <lukelin1991@yahoo.com>
2020-07-07 14:39:15 -0400

    more object annotations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -6,3 +6,40 @@ const subtract = (a: number, b: number): number => {
   // always use a "return" annotation.
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiplay = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({
+  // destructured Annotated TS.
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
