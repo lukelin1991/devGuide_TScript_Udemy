d7e0416febe23b4fc267612bf7669fa5dc9d966a
lukelin1991 <lukelin1991@yahoo.com>
2020-07-07 15:18:03 -0400

    array typescript

---
+++ features/arrays.ts
@@ -0,0 +1,9 @@
+const carMakers = ["ford", "toyota", "chevy"]; // we dont want to have any[] as much as possible.
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+  // only time we annotate is if it is an empty array.
+  ["f150"],
+  ["corolla"],
+  ["camaro"],
+];
