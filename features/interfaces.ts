37d59b984fe8cbea680171fb1452c2e1dbed1114
lukelin1991 <lukelin1991@yahoo.com>
2020-07-08 10:31:29 -0400

    building interfaces to reuse

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,20 +1,29 @@
-interface Vehicle {
+interface Reportable {
   // creating a vehicle..
-  name: string;
-  year: number;
-  broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: "civic",
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year:${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 
-printVehicle(oldCivic);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
